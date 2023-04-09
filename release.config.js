336b5940b8305e781d14c33c85d01cbf228432fc
antoinedao <antoinedao1@gmail.com>
2023-04-09 23:58:52 +0100

    ci(semantic-release): setup config and github action

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/github',
+        ['@semantic-release/exec', {
+            prepareCmd: 'npm run tsc && npm run build',
+        }],
+    ],
+}
