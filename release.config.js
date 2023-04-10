c55ab74656045f2c52d0109d2d25d37d894bdaab
antoinedao <antoinedao1@gmail.com>
2023-04-10 10:12:30 +0100

    ci(semantic-release): add npm config

--- release.config.js
+++ release.config.js
@@ -4,6 +4,7 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/github',
+        '@semantic-release/npm',
         ['@semantic-release/exec', {
             prepareCmd: 'npm run tsc && npm run build',
         }],
